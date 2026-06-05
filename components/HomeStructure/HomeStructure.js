699df31b8839e43f83f95dc6caa9e068f9487992
lmf-git <contact@dreamsbyte.com>
2026-06-05 00:06:16 -0300

    Interactive file explorer

--- components/HomeStructure/HomeStructure.js
+++ components/HomeStructure/HomeStructure.js
@@ -2,6 +2,35 @@ import { Component, define } from '../../core/component.js';
 
 export default class HomeStructure extends Component {
   static templateUrl = '/components/HomeStructure/HomeStructure.html';
+
+  mount() {
+    this._handleClick = this._onTreeClick.bind(this);
+    this.shadowRoot.addEventListener('click', this._handleClick);
+  }
+
+  unmount() {
+    this.shadowRoot?.removeEventListener('click', this._handleClick);
+  }
+
+  _onTreeClick(e) {
+    const row = e.target.closest('.tree-row[data-folder]');
+    if (!row) return;
+
+    const depth = parseInt(row.dataset.depth, 10);
+    const collapsed = row.classList.toggle('collapsed');
+
+    let sibling = row.nextElementSibling;
+    while (sibling) {
+      const sibDepth = parseInt(sibling.dataset.depth, 10);
+      if (sibDepth <= depth) break;
+      if (collapsed) {
+        sibling.hidden = true;
+      } else if (sibDepth === depth + 1) {
+        sibling.hidden = false;
+      }
+      sibling = sibling.nextElementSibling;
+    }
+  }
 }
 
 define('home-structure', HomeStructure);
